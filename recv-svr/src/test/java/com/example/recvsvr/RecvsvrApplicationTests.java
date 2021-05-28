3be92be1051a1d66ff32fee60ba589d007f73d23
luyunxi <luyunxi@huya.com>
2021-05-28 12:56:36 +0800

    init

---
+++ recv-svr/src/test/java/com/example/recvsvr/RecvsvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recvsvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecvsvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
