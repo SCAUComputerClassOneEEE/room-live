4228eca7b7f0a812d9a1f04505a11439bc37c4a6
luyunxi <luyunxi@huya.com>
2021-06-10 10:28:14 +0800

    server beat heart

--- register/src/main/java/com/example/register/RegisterApplication.java
+++ register/src/main/java/com/example/register/RegisterApplication.java
@@ -1,48 +1,89 @@
 package com.example.register;
 
-import com.example.register.server.Server;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.netty.util.CharsetUtil;
 
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 
 //@SpringBootApplication
 public class RegisterApplication {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
+        // 客户端就只需要创建一个 线程组了
+        EventLoopGroup loopGroup = new NioEventLoopGroup();
+        // 创建 启动器
         Bootstrap bootstrap = new Bootstrap();
-        bootstrap.group(new NioEventLoopGroup())
-                .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel socketChannel) throws Exception {
-                        socketChannel.pipeline()
-                                .addLast(new IdleStateHandler(0, 0 ,5, TimeUnit.SECONDS))
-                                .addLast(new SimpleChannelInboundHandler<String>() {
-                                    @Override
-                                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                        ctx.pipeline().channel().writeAndFlush("ccc");
-                                    }
+        try{
+            // 设置相关的参数
+            bootstrap.group(loopGroup)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline()
+                                    .addLast(new IdleStateHandler(0,0,10, TimeUnit.SECONDS))
+                                    .addLast(new NettyClientHandler());
+                        }
+                    });
+            // 连接服务
+            ChannelFuture future = bootstrap.connect("localhost",6668).sync();
+            // 对服务关闭 监听
+            future.channel().closeFuture().sync();
+        }catch (Exception e){
 
-                                    @Override
-                                    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
-                                        System.out.println("client: "+s);
-                                    }
-                                });
-                    }
-                })
-                .connect("127.0.0.1",8081)
-                .addListener(future -> {
-                    System.out.println("client start...");
-                }).sync();
+        }finally {
+            loopGroup.shutdownGracefully();
+        }
+
+    }
+
+    public static class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+        /**
+         * 连接上服务的回调方法
+         * @param ctx
+         * @throws Exception
+         */
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            // 发送数据
+//            System.out.println("连接上了 服务器....");
+//            ctx.writeAndFlush(Unpooled.copiedBuffer("哈哈 你好呀!!!", CharsetUtil.UTF_8));
+        }
+
+        /**
+         * 读取服务端返回的信息
+         * @param ctx
+         * @param msg
+         * @throws Exception
+         */
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//            ByteBuf buf  = (ByteBuf) msg;
+//            System.out.println("服务端返回的信息：" + buf.toString(CharsetUtil.UTF_8));
+            //ctx.writeAndFlush(Unpooled.copiedBuffer("哈哈 你好呀!!!", CharsetUtil.UTF_8));
+        }
+
+        @Override
+        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+            if (evt instanceof IdleStateEvent) {
+                IdleStateEvent event = (IdleStateEvent) evt;
+                if (event.state() == IdleState.ALL_IDLE) {
+                    ctx.channel().writeAndFlush(Unpooled.copiedBuffer("heartbeat", CharsetUtil.UTF_8));
+                }
+            }
+        }
     }
 
 }
--- register/src/main/java/com/example/register/server/RegisterServer.java
+++ register/src/main/java/com/example/register/server/RegisterServer.java
@@ -1,15 +1,16 @@
 package com.example.register.server;
 
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
+import io.netty.util.CharsetUtil;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,8 +19,87 @@ import java.util.concurrent.TimeUnit;
  * @since 2021/6/9 22:02
  **/
 public class RegisterServer {
-    public static void main(String[] args) throws InterruptedException {
-        Server server = new Server();
-        server.start(8081);
+    public static void main(String[] args) {
+        // 创建对应的 线程池
+        // 创建Boss group
+        EventLoopGroup boosGroup = new NioEventLoopGroup(1);
+        // 创建 workgroup
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        // 创建对应的启动类
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        try{
+            // 设置相关的配置信息
+            bootstrap.group(boosGroup,workGroup) // 设置对应的线程组
+                    .channel(NioServerSocketChannel.class) // 设置对应的通道
+                    .option(ChannelOption.SO_BACKLOG,1024) // 设置线程的连接个数
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // 设置
+                        /**
+                         * 给pipeline 设置处理器
+                         * @param socketChannel
+                         * @throws Exception
+                         */
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline()
+                                    .addLast(new IdleStateHandler(0,0,10, TimeUnit.SECONDS))
+                                    .addLast(new NettyServerHandler());
+                        }
+                    });
+            System.out.println("服务启动了....");
+            // 绑定端口  启动服务
+            ChannelFuture channelFuture = bootstrap.bind(6668).sync();
+            // 对关闭通道进行监听
+            channelFuture.channel().closeFuture().sync();
+        }catch (Exception e){
+
+        }finally {
+            // 优雅停服
+            boosGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+    }
+
+    public static class NettyServerHandler extends ChannelInboundHandlerAdapter {
+        private int lossConnectCount = 0;
+        /**
+         * 读取客户端发送来的数据
+         * @param ctx ChannelHandler的上下文对象 有管道 pipeline 通道 channel 和 请求地址 等信息
+         * @param msg 客户端发送的具体数据
+         * @throws Exception
+         */
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf buf = (ByteBuf) msg;
+            System.out.println("客户端发送的数据是:" +buf.toString(CharsetUtil.UTF_8));
+
+        }
+
+        /**
+         * 读取客户端发送数据完成后的方法
+         *    在本方法中可以发送返回的数据
+         * @param ctx
+         * @throws Exception
+         */
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+
+        }
+
+        @Override
+        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+            if (evt instanceof IdleStateEvent){
+                IdleStateEvent event = (IdleStateEvent)evt;
+                if (event.state()== IdleState.ALL_IDLE){
+                    lossConnectCount ++;
+                    if (lossConnectCount > 2){
+                        System.out.println("关闭这个不活跃通道！");
+                        ctx.channel().close();
+                    }
+                }
+            }else {
+                super.userEventTriggered(ctx,evt);
+            }
+        }
     }
 }
